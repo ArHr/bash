bc8d7d40c61405667a60e99d5e1e462ecb542397
ArHr <aromic@gmail.com>
2015-03-10 00:52:30 +0100

    cpdomain

---
+++ cpdomain.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function get_domain {
+
+}
+
+function get_webroot {
+
+}
+
+function go_cwd {
+    
+}
+
+#grep -B 3 -A 3 $domain /usr/local/apache/conf/httpd.conf | grep Root
+    DocumentRoot /home/ionaviat/public_html
+
